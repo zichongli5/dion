1eb6209ac5bbc9b14145c7fb9fedc87b079a7c66
zichongli5 <zichongli5@gmail.com>
2025-11-24 18:15:40 -0500

    debug

--- dion/__init__.py
+++ dion/__init__.py
@@ -5,4 +5,4 @@ from .dion_reference import Dion as DionReference
 from .muon import Muon
 from .muon_reference import Muon as MuonReference
 from .dion2 import Dion2
-from .normuon import Normuon
+from .normuon import NorMuon
